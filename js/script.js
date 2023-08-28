f4e5a93d658a5513101cad14983a26d7d130c9e8
thetechgirl01 <joychisom1580@gmail.com>
2023-08-28 14:42:55 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,12 +1,56 @@
-const toggleBtn = document.querySelector('.toggle-btn')
-const toggleBtnIcon = document.querySelector('.toggle-btn i')
-const dropDownMenu = document.querySelector('.dropdown-menu')
 
-toggleBtn.oneclick = function () {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
 
-    toggleBtnIcon.classList = isOpen
-    ? 'fa-solid fa-xmark'
-    : 'fa-solid fa-bars'
-}
+//sign up //
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // const signupForm = document.getElementById('sign-up');
+     
+     // Get a reference to the signup button using its class
+     const signupButton = document.getElementById('signup-button');
+     
+     signupButton.addEventListener('click', async function(event) {
+         event.preventDefault();
+         
+         const firstName = document.getElementById('first-name').value;
+         const lastName = document.getElementById('last-name').value;
+         const email = document.getElementById('email').value;
+         const password = document.getElementById('password').value;
+ 
+         const formData = {
+             firstName: firstName,
+             lastName: lastName,
+             email: email,
+             password: password
+         };
+         console.log(formData)
+
+         try {
+             const response = await fetch('https://globe-blog.onrender.com/api/v1/users', {
+                 method: 'POST',
+                 headers: {
+                     'Content-Type': 'application/json'
+                 },
+                 body: JSON.stringify(formData)
+             });
+ 
+             const responseData = await response.json();
+ 
+             console.log(response)
+ 
+             if (response.status === 200) {
+                 console.log('User registered successfully:', responseData.message);
+                 // Redirect to a success page or display a success message
+             } else if (response.status === 409) {
+                 console.log('User already exists:', responseData.message);
+                 // Display a message indicating the user already exists
+             } else {
+                 console.log('An error occurred:', responseData.message);
+                 // Display a general error message
+             }
+         } catch (error) {
+             console.error('An error occurred:', error);
+         }
+     });
+ });
